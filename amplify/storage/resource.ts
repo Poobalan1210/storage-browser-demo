02e651ad54e32f441255fff4040d6c3f8cea515e
Poobalan <poobalan12100@gmail.com>
2025-02-07 23:47:13 +0530

    no message

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,15 +3,12 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'myS3Bucket',
   access: (allow) => ({
-    'public/*': [
+    'images/*': [
       allow.guest.to(['read']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    'protected/{entity_id}/*': [
+    'docs/*': [
       allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
